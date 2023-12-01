d089f7ab0c47a87152f4a59fc68940b569fe7fe2
Eduardo ML <edu@eml.run>
2023-12-01 01:48:35 -0600

    fix script env generator

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  secretKey: 'millavesecreta',
+  appVersion: '0.0.0',
+  production: true,
+};
--- src/utils/setEnv.ts
+++ src/utils/setEnv.ts
@@ -1,28 +1,47 @@
+/* tslint:disable */
+// @ts-nocheck
+const { writeFile, existsSync, mkdirSync } = require('fs');
+require('dotenv').config()
 const setEnv = () => {
-  const fs = require('fs');
-  const writeFile = fs.writeFile;
-// Configure Angular `environment.ts` file path
+  // Configure Angular `environment.ts` file path
   const targetPath = './src/environments/environment.ts';
-// Load node modules
+
+  // Providing path to the `environments` directory
+  const envDirectory = './src/environments';
+
+  // creates the `environments` directory if it does not exist
+  if (!existsSync(envDirectory)) {
+    mkdirSync(envDirectory);
+  }
+
+  // Load node modules
   const colors = require('colors');
   const appVersion = require('../../package.json').version;
   require('dotenv').config({
-    path: 'src/environments/.env'
+    path: 'src/environments/.env',
   });
-// `environment.ts` file structure
+  // `environment.ts` file structure
   const envConfigFile = `export const environment = {
-  googleApiKey: '${process.env['SECRET_KEY']}',
+  secretKey: '${process.env.SECRET_KEY}',
   appVersion: '${appVersion}',
   production: true,
 };
 `;
-  console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+  console.log(
+    colors.magenta(
+      'The file `environment.ts` will be written with the following content: \n'
+    )
+  );
   writeFile(targetPath, envConfigFile, (err: any) => {
     if (err) {
       console.error(err);
       throw err;
     } else {
-      console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+      console.log(
+        colors.magenta(
+          `Angular environment.ts file generated correctly at ${targetPath} \n`
+        )
+      );
     }
   });
 };
